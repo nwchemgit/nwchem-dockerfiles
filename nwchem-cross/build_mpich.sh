fe8fb6b85ae1a1fb25704a0f7403417171624d02
edoapra <edoardo.apra@gmail.com>
2023-07-01 07:54:10 -0700

    cross32 for mpich

--- nwchem-cross/build_mpich.sh
+++ nwchem-cross/build_mpich.sh
@@ -49,7 +49,14 @@ fi
 if [ -x "$(command -v xx-info)" ]; then
     SHARED_FLAGS+=" --host=$(xx-info triple) "
     SHARED_FLAGS+=" --build=$(uname -m)-linux-gnu "
-    SHARED_FLAGS+=" --with-cross=../cross.txt "
+    # 32bit or 64bit arch?
+    arch=$(xx-info march)
+    echo "mpich arch is " $arch
+    if echo $arch |grep -q 32 ; then
+	SHARED_FLAGS+=" --with-cross=../cross32.txt "
+    else
+	SHARED_FLAGS+=" --with-cross=../cross.txt "
+    fi
 fi
 echo SHARED_FLAGS is $SHARED_FLAGS
  ./configure --prefix=`pwd`/../.. --enable-fortran=all $SHARED_FLAGS  --disable-cxx --enable-romio --with-pm=gforker --with-device=ch3:nemesis --disable-cuda --disable-opencl --enable-silent-rules  --enable-fortran=all 
